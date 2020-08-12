9b01e680fecd8f992468dcfc8b04627de1c61ead
bummings <eabummings@gmail.com>
2020-08-12 14:17:27 -0400

    added fundamental routes

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('This is the root.');
+});
+
+app.get('/test', (req, res) => {
+  res.send('This is a test route.');
+});
+
 app.listen(port, () => {
-  console.log(`Up and running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
