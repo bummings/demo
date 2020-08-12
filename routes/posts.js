26c2bd51778e310b8d44a5f12083e6382a9cf970
bummings <eabummings@gmail.com>
2020-08-12 16:25:55 -0400

    creates post model

--- routes/posts.js
+++ routes/posts.js
@@ -8,4 +8,9 @@ router.get('/', (req, res) => {
   res.send('Here are the posts');
 });
 
+// example of using router middleware for a sub-post page
+router.get('/specific', (req, res) => {
+  res.send('VERY specific post');
+});
+
 module.exports = router;
